54e435bb8a8d411ff0923d92b05807300d84554d
Marcelle Dantas <marcellemoraesdantas@gmail.com>
2022-03-25 17:30:39 -0300

    Updates

--- src/POO/TesteCarro.java
+++ src/POO/TesteCarro.java
@@ -1,7 +1,40 @@
 package POO;
 
 public class TesteCarro {
-    public static void main(String[] args){
+    //Classe de teste carro
 
+
+    public static void main(String[] args){
+        //Carro carro = new Carro();
+        //Métodos static não é necessário instanciar para se utilizar:
+        // ex: public String getMarca() :: é necessário instanciar Carro car = new Carro()
+        //Já declarando public static String getMarca() :: não é necessário instanciar :: apenas usar Carro.getMarca();
+
+        Carro carro = new Carro();
+
+        carro.getMarca();
+        carro.setMarca("Fiat");
+
+        carro.getNumPassageiros();
+        carro.setNumPassageiros(10);
+
+        carro.getconsumoCombustivel();
+        carro.setconsumoCombustivel(100);
+
+        carro.getcapCombustivel();
+        carro.setcapCombustivel(4);;
+
+
+        carro.exibirAutonomia(); //Método sem retorno
+        System.out.println("Autonomia do carro " + carro.obterAutonomia()); // Método com retorno
+        System.out.println("Quantidade de combustível " + carro.calculaCombustivel(2)); // Método com retorno e parâmetro
+
+        System.out.println("Carro 1: marca " + carro.getMarca());
+        auxiliarStatic(); //acesso na main porque ele é STATIC
+    }
+
+     static void auxiliarStatic (){
+        System.out.println("Quero acessar esse método na main, portanto ele deve ser estático");
     }
 }
+
---
+++ src/Test/Teste.java
@@ -0,0 +1,13 @@
+package Test;
+
+import POO.Carro;
+
+public class Teste {
+
+    public static void main (String[] args){
+//        Carro carro = new Carro();
+//        carro.marca = "Fiat";
+//        carro.modelo = "Ducato";
+    }
+}
+
