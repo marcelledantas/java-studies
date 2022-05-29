f5112ef6372013d444a8b726586f81e82843bc57
Marcelle Dantas <marcellemoraesdantas@gmail.com>
2022-05-28 22:07:10 -0300

    add

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+/**
+ * @author marcelle.dantas
+ *
+ */
+public class Main {
+
+    public static void main(String[] args) {
+//
+//
+
+    }
+}
--- src/Point.java
+++ src/Point.java
@@ -1,3 +1,35 @@
+
 public class Point {
+
+    private int x;
+    private int y;
+
+    Point(){ };
+
+    Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distance (){
+        double dist;
+        dist = Math.sqrt( (this.x - 0.0) * (this.x - 0.0) + (this.y));
+        return dist;
+    }
 
 }
