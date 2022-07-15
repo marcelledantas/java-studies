4c61afbd33c514315ee56c7c4750e938b5ae08ff
Marcelle Dantas <marcellemoraesdantas@gmail.com>
2022-07-14 22:01:10 -0300

    Sorting an array

--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -7,49 +7,49 @@ public class Main {
 
 
     public static void main(String[] args) {
-//        int[] myIntegers = getIntegers(5);
-//
-//        for(int i = 0; i < myIntegers.length; i++){
-//            System.out.println("myIntegers value = " + myIntegers[i] + " element i = " + i);
-//        }
-//
-//        System.out.println("Average of myIntegers arrays = " + getAverage(myIntegers));
-//
-//        int[] myIntVariables = {1,2,3,4,5,6,7,8,9,10};
-//
-//        double[] myDoubleArray = new double[10];
+        int[] myIntegers = getIntegers(5);
 
-//        for(int i = 0; i < myDoubleArray.length; i++){
-//            myDoubleArray[i] = i * 10;
-//            System.out.println("i = " + i + "  myDoubleArray[i] = " + myDoubleArray[i]);
-//        }
+        for(int i = 0; i < myIntegers.length; i++){
+            System.out.println("myIntegers value = " + myIntegers[i] + " element i = " + i);
+        }
 
-//        for(int i = 0; i < 10; i++){
-//            myDoubleArray[i] = i * 10;
-//            System.out.println("i = " + i + "  myDoubleArray[i] = " + myDoubleArray[i]);
-//        }
+        System.out.println("Average of myIntegers arrays = " + getAverage(myIntegers));
+
+        int[] myIntVariables = {1,2,3,4,5,6,7,8,9,10};
+
+        double[] myDoubleArray = new double[10];
+
+        for(int i = 0; i < myDoubleArray.length; i++){
+            myDoubleArray[i] = i * 10;
+            System.out.println("i = " + i + "  myDoubleArray[i] = " + myDoubleArray[i]);
+        }
+
+        for(int i = 0; i < 10; i++){
+            myDoubleArray[i] = i * 10;
+            System.out.println("i = " + i + "  myDoubleArray[i] = " + myDoubleArray[i]);
+        }
 
 //        printArray(myDoubleArray);
-//
-//        int[] myIntArray = new int[10];
-//
-//        System.out.println(myIntVariables[5]);
 
-        int[] myIntegers = getIntegers(5);
+        int[] myIntArray = new int[10];
+
+        System.out.println(myIntVariables[5]);
+
+//        int[] myIntegers = getIntegers(5);
         int[] myIntegersSorted = sortArray(myIntegers);
         printArray(myIntegersSorted);
     }
 
 
-//    public static int getAverage (int[] array){
-//        int sum = 0;
-//
-//        for(int i = 0; i < array.length; i++){
-//            sum += array[i];
-//        }
-//
-//        return sum/array.length;
-//    }
+    public static int getAverage (int[] array){
+        int sum = 0;
+
+        for(int i = 0; i < array.length; i++){
+            sum += array[i];
+        }
+
+        return sum/array.length;
+    }
 
 
     public static int[] getIntegers(int n){
