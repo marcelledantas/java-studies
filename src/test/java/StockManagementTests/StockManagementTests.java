701c256d73a51cbfaf34a3896d3896bf67519d7b
marcelledantas <marcellemoraesdantas@gmail.com>
2023-01-03 16:58:23 -0300

    Notes about mocks

--- src/test/java/StockManagementTests/StockManagementTests.java
+++ src/test/java/StockManagementTests/StockManagementTests.java
@@ -6,13 +6,14 @@ import StockManagement.StockManager;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class StockManagementTests {
 
     @Test
     public void testCanGetACorrectLocatorCode () {
 
-        ExternalISBNDataService testService = new ExternalISBNDataService() {
+        ExternalISBNDataService testWebService = new ExternalISBNDataService() {
             @Override
             public Book lookup(String isbn) {
                 return new Book("J. Steinbeck", "Of Mice and Men", isbn);
@@ -23,12 +24,44 @@ public class StockManagementTests {
         // And you can't create this kind of interface. Or there might be times when the interface is so big and so difficult that actually creating it with the local implementation is a huge amount of work.
         // So we are going to look forward at other ways of dealing with this using a third party library
 
+        ExternalISBNDataService testDatabaseService = new ExternalISBNDataService() {
+            @Override
+            public Book lookup(String isbn) {
+                return null;
+            }
+        };
+
         StockManager stockManager = new StockManager();
-        stockManager.setService(testService);
+        stockManager.setWebService(testWebService);
+        stockManager.setDatabaseService(testDatabaseService);
 
 
         String isbn = "0140177396";
         String locatorCode = stockManager.getLocatorCode(isbn);
         assertEquals("7396J4", locatorCode);
     }
+
+    @Test
+    public void databaseIsUsedIfDataIsPresent(){
+        // For creating a mocking rather than instantiate it, we are going to set to equal then the method mock(), passing the class type that we want to mock.
+        // It creates a dummy class that is an implementation for this interface. We don't need to provide an implementation for the lookup method.
+        ExternalISBNDataService databaseService = mock(ExternalISBNDataService.class);
+        ExternalISBNDataService webService = mock(ExternalISBNDataService.class);
+
+        when(databaseService.lookup("0140177396")).thenReturn(new Book("abc", "abc", "0140177396"));
+
+        StockManager stockManager = new StockManager();
+        stockManager.setWebService(webService);
+        stockManager.setDatabaseService(databaseService);
+
+
+        String isbn = "0140177396";
+        String locatorCode = stockManager.getLocatorCode(isbn);
+        assertEquals("7396J4", locatorCode);
+
+    }
+
+    @Test
+    public void webServiceIsUsedIfDataIsNotPresentInDatabase(){}
+
 }
