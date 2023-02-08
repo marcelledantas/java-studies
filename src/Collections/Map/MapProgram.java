7c4ce658d336d68c08c19589122c458a56139d92
marcelledantas <marcellemoraesdantas@gmail.com>
2023-02-07 23:42:26 -0300

    maps and its methods

--- src/Collections/Map/MapProgram.java
+++ src/Collections/Map/MapProgram.java
@@ -9,17 +9,17 @@ public class MapProgram {
 
         Map<String, String> languages = new HashMap<>();
 
+        languages.put("Python", "an interpreted, object-oriented, high-level programming language");
+
         if(languages.containsKey("Java")){
             System.out.println("Java already exists");
         } else{
             languages.put("Java", "a compiled high level, object-oriented, platform independent language");
             System.out.println("Java added successfully");
         }
-
-        languages.put("Python", "an interpreted, object-oriented, high-level programming language");
         System.out.println(languages.put("Algol", "an algorithmic language"));
         System.out.println(languages.put("BASIC", "Beginners ALL Purposes Symbolic Instruction Code"));
-
+        languages.put("Lisp", "Therein lies madness");
 //        if(languages.containsKey("Java")){
 //            System.out.println("Java is already in the map");
 //        } else{
@@ -30,6 +30,20 @@ public class MapProgram {
 //        System.out.println(languages.get("Java")); //overriden
 
         System.out.println("=====================================================");
+
+//        languages.remove("Lisp");
+        if(languages.remove("BASIC", "Beginners ALL Purposes Symbolic Instruction Code")){
+            System.out.println("BASIC removed");
+        } else{
+            System.out.println("BASIC not removed, key/value pair not found");
+        }
+
+        if((languages.replace("Lisp", "Therein lies madness" ,"a functional programming language with imperative features"))){
+            System.out.println("Lisp replaced");
+        } else{
+            System.out.println("List was not replaced");
+        }
+        System.out.println(languages.replace("Scala", "this will not be added"));
 
         for(String key: languages.keySet()){
             System.out.println( key + " : " + languages.get(key));
