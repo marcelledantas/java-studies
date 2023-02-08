75e46f5220631cfd3420377d745e2b895b013ecc
marcelledantas <marcellemoraesdantas@gmail.com>
2023-02-08 16:54:19 -0300

    the adventures game and split method

--- src/Collections/Map/Adventure/Location.java
+++ src/Collections/Map/Adventure/Location.java
@@ -12,6 +12,7 @@ public class Location {
         this.locationID = locationID;
         this.description = description;
         this.exists = new HashMap<String, Integer>();
+        addExit("Q", 0);
     }
 
     public void addExit(String direction, int location){
--- src/Collections/Map/Adventure/Main.java
+++ src/Collections/Map/Adventure/Main.java
@@ -17,17 +17,60 @@ public class Main {
         locations.put(4, new Location(4,"You are in a valley beside a stream"));
         locations.put(5, new Location(5,"You are in the forest"));
 
-        int loc = 1;
-        while(true){
-            System.out.println(locations.get(loc).getDescription());
-            if(loc == 0){
-                break;
-            }
+        locations.get(1).addExit("W", 2);
+        locations.get(1).addExit("E", 3);
+        locations.get(1).addExit("N", 5);
+        locations.get(1).addExit("S", 4);
+//        locations.get(1).addExit("Q", 0);
 
-            loc = scanner.nextInt();
-            if(!locations.containsKey(loc)){
-                System.out.println("You cannot go in the direction");
-            }
+        locations.get(2).addExit("N", 5);
+//        locations.get(2).addExit("Q", 0);
+
+        locations.get(3).addExit("W", 1);
+//        locations.get(3).addExit("Q", 0);
+
+        locations.get(4).addExit("N", 1);
+        locations.get(4).addExit("W", 2);
+//        locations.get(4).addExit("Q", 0);
+
+        locations.get(5).addExit("S", 1);
+        locations.get(5).addExit("W", 2);
+//        locations.get(5).addExit("Q", 0);
+
+
+
+        int loc = 1;
+//        while(true){
+//            System.out.println(locations.get(loc).getDescription());
+//            if(loc == 0){
+//                break;
+//            }
+//
+//            Map<String, Integer> exists = locations.get(loc).getExists();
+//            System.out.println("Available exists are ");
+//            for(String exit: exists.keySet()){
+//                System.out.print(exit + ", ");
+//            }
+//            System.out.println();
+//
+//            String directions = scanner.nextLine().toUpperCase();
+//            if(exists.containsKey(directions)){
+//                loc = exists.get(directions);
+//            } else{
+//                System.out.println("You cannot go in that direction");
+//            }
+//        }
+
+        String[] road = "You are standing at the end of a road before a small brick building".split(" ");
+        for(String i : road){
+            System.out.println(i);
+        }
+
+        System.out.println("===============================");
+
+        String[] building = "You are standing inside a building, a well house for a small spring".split(", ");
+        for(String i : building){
+            System.out.println(i);
         }
 
     }
