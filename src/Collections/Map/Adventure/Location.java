13c264e73157081c97dd29aaf587da34719b99c6
marcelledantas <marcellemoraesdantas@gmail.com>
2023-02-08 00:20:39 -0300

    Starting with the adventures game

---
+++ src/Collections/Map/Adventure/Location.java
@@ -0,0 +1,32 @@
+package Collections.Map.Adventure;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Location {
+    private final int locationID;
+    private final String description;
+    private final Map<String, Integer> exists;
+
+    public Location(int locationID, String description) {
+        this.locationID = locationID;
+        this.description = description;
+        this.exists = new HashMap<String, Integer>();
+    }
+
+    public void addExit(String direction, int location){
+        exists.put(direction, location);
+    }
+
+    public int getLocationID() {
+        return locationID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Map<String, Integer> getExists() {
+        return new HashMap<>(exists); // Nothing outside this class will be able to change exits, because get will create a new object
+    }
+}
